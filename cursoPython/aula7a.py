8e390ee803815d45ba0263dac925a6ac421cd9d4
Diego Hartwig Brandl <diegohbrandl@gmail.com>
2020-05-12 12:42:14 -0300

    operadores

---
+++ cursoPython/aula7a.py
@@ -0,0 +1,9 @@
+n1 = int(input('digite o valor 1 >>> '))
+n2 = int(input('Digite o valor 2 >>> '))
+s = n1 + n2
+m = n1 - n2
+mu = n1 * n2
+d = n1 / n2
+e = n1 ** n2
+di = n1 // n2
+print('A soma >> {}\n subtração >> {}\n multiplicação >> {}\n divisão >> {}\n elevação >> {}\n divisão inteira >> {}\n'.format(s, m, mu, d, e, di))
